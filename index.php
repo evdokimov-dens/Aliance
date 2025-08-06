08e1fee3819ea0d7d5ef3b0f704c1723a35c3c97
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-08-06 22:04:04 +0500

    add: php in footer

--- index.php
+++ index.php
@@ -627,7 +627,7 @@
        <div class="container">
         <div class="footer-wrapper">
           <div class="footer-legal">
-          <p class="footer-copyright">&copy; 2022 «Aliance Production». Все права защищены.</p>
+          <p class="footer-copyright">&copy; <?php echo date ('Y');?> «Aliance Production». Все права защищены.</p>
           <a href="#" class="footer-policy">Политики конфиденциальности</a>
         </div>
         <!-- /.footer-legal -->
